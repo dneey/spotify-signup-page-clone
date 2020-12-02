5d72387e88d41c88c674d0c17a45330ccfc78975
David Yartey <yarteyd@gmail.com>
2020-12-02 21:30:05 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+
+  purge: [],
+  theme: {
+    extend: {
+
+      outline: {
+        green: '1px solid green',
+      },
+      fontSize: {
+        'tiny' : '0.10rem',
+        'xl': '1.25rem',
+        '2.5xl': '1.825rem',
+        '3xl': '1.875rem',
+       },
+       spacing: {
+        '28': '7.00rem',
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+        '108': '27rem',
+        '120': '30rem',
+        '132': '33rem',
+        '144': '37rem',
+        '156': '40rem',
+        '216': '54rem',
+      }
+
+
+    },
+  },
+  variants: {
+
+    padding: ['responsive','hover', 'focus', 'active'],
+    margin: ['responsive','hover', 'focus', 'active'],
+    backgroundColor: ["responsive", "hover", "focus", "active"],
+    boxShadow: ["responsive", "hover", "focus"],
+  },
+  plugins: [],
+};
